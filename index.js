4844831a7457fdc23191a3de3dac753ca6bbf65b
Damir Vieth <vieth.damir@gmail.com>
2023-08-03 15:10:18 +0200

    extra stream

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
-const axios = require('axios');
+const axios = require('axios').default;
+const stream = require('stream');
 
 module.exports = async (req, res) => {
   const { url } = req.query;
 
   if (!url) {
-    return res.status(400).send('URL is required');
+    return res.status(400).send('URL is required!');
   }
 
   try {
-    const response = await axios.get(url);
-    
+    const response = await axios.get(url, {
+      responseType: 'stream'
+    });
+
     // Set CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    
-    // Set the content type from the original response
+
+    // Set Content-Type header to match the original response
     res.setHeader('Content-Type', response.headers['content-type']);
 
-    res.json(response.data);
+    // If the response content type is JSON, convert the stream to JSON and send it
+    if (response.headers['content-type'].includes('application/json')) {
+      let data = '';
+      response.data.on('data', chunk => data += chunk);
+      response.data.on('end', () => res.json(JSON.parse(data)));
+    } else {
+      // Otherwise, pipe the response data as a stream
+      response.data.pipe(res);
+    }
+
   } catch (error) {
     res.status(500).send('Error fetching data');
   }
 };
+
 
 
 
