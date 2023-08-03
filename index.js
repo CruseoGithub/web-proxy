d673dacb946d299f6c259f0fcff5c6c9b386fe0d
Damir Vieth <vieth.damir@gmail.com>
2023-08-03 13:39:46 +0200

    fix cors

--- index.js
+++ index.js
@@ -9,11 +9,18 @@ module.exports = async (req, res) => {
 
   try {
     const response = await axios.get(url);
+    
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    
     res.json(response.data);
   } catch (error) {
     res.status(500).send('Error fetching data');
   }
 };
+
 
 
 /* const express = require('express');
